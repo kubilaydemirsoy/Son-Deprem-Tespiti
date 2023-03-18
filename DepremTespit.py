c39c242433e529dd2bec02532b775c210709167a
Kubilay Demirsoy <92679666+kubilaydemirsoy@users.noreply.github.com>
2023-03-18 14:01:27 +0300

    Son Deprem Tespiti

---
+++ DepremTespit.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+url = "http://www.koeri.boun.edu.tr/scripts/lst0.asp"
+response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content)
+pre = soup.find("pre")
+text = pre.text
+liste = text.split("\n")
+print("\n.".join(liste[4:8]))
